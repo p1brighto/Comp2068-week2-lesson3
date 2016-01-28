a4eead4e509668f15f70da21a7d5c0da6fd78fe7
p1brighto <p1brighto@gmail.com>
2016-01-28 15:37:41 -0500

    second test

--- server.ts
+++ server.ts
@@ -1,5 +1,27 @@
 ///<refernce path="_reference.ts"/>
-import http=require('http');
+import express = require('express');
+
+var app:express.Express = express();
+
+var port:number = process.env.port || 3000;
+
+// Main route
+app.get('/', 
+    function (req:express.Request, res:express.Response, next:any) {
+    res.send('Hello World!');
+});
+
+// info route
+app.get('/info', 
+    function (req:express.Request, res:express.Response) {
+    res.send('Info Page');
+});
+
+app.listen(port, function () {
+  console.log('Example app listening on port', + port);
+});
+
+/*import http=require('http');
 
 var port:number=process.env.port|| 3000;
 var server:http.Server=http.createServer(function(req:http.ServerRequest,res:http.ServerResponse){
@@ -9,4 +31,4 @@ var server:http.Server=http.createServer(function(req:http.ServerRequest,res:htt
 
 server.listen(port,function(){
         console.log("Server Strated....Listening on port: "+port);
-});
+});*/
